6e19ebfddb61b5bd7981c3d4961ea64961aabb96
Sam <sgluss@mail.sfsu.edu>
2016-03-12 14:46:13 -0800

    started interpolation assignment

---
+++ Interpolation/hermiteInterpolation.py
@@ -0,0 +1,6 @@
+#   Samuel Gluss                        #
+#   3/12/2016                           #
+#   Math 400 Assignment 2 part 2        #
+#   Generalized Hermite Interpolation   #
+#########################################
+
