0276cfce874fa16875fa24aad83fb42779d1f7cd
Sam <sgluss@mail.sfsu.edu>
2016-03-20 02:29:44 -0700

    started work on lagrange based hermite interpolation, abandoned divided difference table as per request of professor David Sklar. Code archived for posterity

--- Interpolation/hermiteInterpolation.py
+++ Interpolation/hermiteInterpolation.py
@@ -12,47 +12,16 @@ points = [[-1.,2.,-8.,56.],
           [0.,1.,0.,0.],
           [1.,2.,8.,56.]]
 
-def getDDTableDiagonal(points):
-    rows = 3 * len(points)
-    #   create a divided differences table
-    ddTable = [[points[trunc(row/3)][0],
-                points[trunc(row/3)][1]]
-               for row in range(0,rows)]
-
-    #   populate first derivatives
-    for row in range(0,rows - 1):
-        diff = ddTable[row+1][1] - ddTable[row][1]
-        if diff == 0:
-            ddTable[row].append(points[trunc(row/3)][2])
-        else:
-            diff /= (ddTable[row+1][0] - ddTable[row][0])
-            ddTable[row].append(diff)
-    ddTable[-1].append(0)
-
-    #   populate second derivatives
-    for row in range(0,rows - 2):
-        diff = ddTable[row+1][2] - ddTable[row][2]
-        if diff == 0:
-            ddTable[row].append(points[trunc(row/3)][3] / 2)
-        else:
-            diff /= (ddTable[row+2][0] - ddTable[row][0])
-            ddTable[row].append(diff)
-    ddTable[-2].append(0)
-    ddTable[-1].append(0)
-
-    #   populate remainder of table
-    for col in range(4, rows + 1):
-        for row in range(0,rows):
-            if row > rows - col:
-                ddTable[row].append(0)
-            else:
-                diff = ddTable[row+1][col-1] - ddTable[row][col-1]
-                diff /= (ddTable[(row+col)-1][0] - ddTable[row][0])
-                ddTable[row].append(diff)
-    #   return the 'diagonal' of the table
-    return(ddTable)
-
-ddTable = getDDTableDiagonal(points)
-ddRows = ddTable[0][1:]
+    #               (x - x0) . . .(x - xk-1)(x - xk+1) . . .(x - xn)
+    #   Ln,k(x) =   --------------------------------------------------
+    #               (xk - x0) . . .(xk - xk-1)(xk - xk+1) . . .(xk - xn)
+def nthLagrange(n,k,x,xlist):
+    numerator = 1
+    denominator = 1
+    for i in range(0,len(xlist)):
+        if(i != k):
+            numerator *= (x - xlist[i])
+            denominator *= (xlist[k] - xlist[i])
+    return numerator / denominator
 
 print("Execution Complete")
